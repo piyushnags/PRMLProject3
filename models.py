ee92ac6e2d52fdd96b68ba42a99c1466fc17b7c1
Piyush <piyushnags@gmail.com>
2023-03-06 15:18:28 -0500

    ref

--- models.py
+++ models.py
@@ -28,16 +28,11 @@ class MLP(nn.Module):
         """
         super(MLP, self).__init__()
         self.layers = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim//2),
+            nn.Linear(input_dim, hidden_dim),
 
-            nn.Linear(hidden_dim//2, hidden_dim//2),
-            nn.LeakyReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
 
-            nn.Linear(hidden_dim//2, hidden_dim//2),
-            nn.LeakyReLU(),
-            
-            nn.Linear(hidden_dim//2, output_dim),
-            nn.ReLU()
+            nn.Linear(hidden_dim, output_dim),
         )
 
     def forward(self, x):
