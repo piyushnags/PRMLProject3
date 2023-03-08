119bbe59d17ae4237bf2237036ade6f1e085d221
Piyush <piyushnags@gmail.com>
2023-03-08 16:38:12 -0500

    fixed resnet augmentation

--- classification.py
+++ classification.py
@@ -342,10 +342,7 @@ def resnet_main(args):
         transforms.Resize((224, 224)),
         transforms.Grayscale(),
         transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        ),
+        transforms.Normalize((0.5, ), (0.5, )),
     ]
     augmentation = [
         transforms.RandomRotation(degrees=(0, 360)),
